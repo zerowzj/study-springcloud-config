e485d9d00b8818ab6dd1377d3e80847b2b5654aa
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-11-20 14:30:00 +0800

    提交代码

--- src/main/java/study/springcloud/config/support/SpringBootCfg.java
+++ src/main/java/study/springcloud/config/support/SpringBootCfg.java
@@ -1,7 +1,9 @@
 package study.springcloud.config.support;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication(scanBasePackages = "study.springcloud.config")
 public class SpringBootCfg {
 
